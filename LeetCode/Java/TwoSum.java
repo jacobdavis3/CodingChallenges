b8a2170cbf003436eefedc552bf3a40cd9b4daca
Jacob Davis <jd3728@princeton.edu>
2024-06-18 14:45:57 +0300

    hash map for the first time

--- LeetCode/Java/TwoSum.java
+++ LeetCode/Java/TwoSum.java
@@ -1,4 +1,12 @@
-import java.util.ArrayList;
+/***************************************************************************************************
+ * 
+ * Given an array of integers nums and an integer target, return indices of the two numbers such 
+ * that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same 
+ * element twice.
+ *
+ ***************************************************************************************************/
 import java.util.HashMap;
 
 public class TwoSum {
